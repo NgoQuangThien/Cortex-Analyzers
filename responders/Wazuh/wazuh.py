92c03172af6df6bcabf237784c0c8fc52587e049
Ngo Thien <quangthien18032000@gmail.com>
2023-03-12 21:06:30 +0700

    Update analyzers and responder

--- responders/Wazuh/wazuh.py
+++ responders/Wazuh/wazuh.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+
 from cortexutils.responder import Responder
 from base64 import b64encode
 import requests
@@ -10,6 +12,8 @@ import json
 class Wazuh(Responder):
     def __init__(self):
         Responder.__init__(self)
+
+        # Service to be executed
         self.service = self.get_param( "config.service", None, "Service parameter is missing" )
 
         # Wazuh server access information
@@ -54,14 +58,14 @@ class Wazuh(Responder):
             response = self.session.get(
                 f"{api_url}", headers=login_headers, verify=False, timeout=3
             )
-        except requests.ConnectionError:
-            self.error({'message': "Login failed!"})
+        except requests.ConnectionError as e:
+            self.error("Connection failed: " + str(e))
 
         if response.status_code == 200:
             token = json.loads(response.content.decode())["data"]["token"]
             self.session.headers.update({'Authorization': f'Bearer {token}'})
         else:
-            self.error({'message': "Login failed!"})
+            self.error("Login failed with status code: " + str(response.status_code))
 
 
     def session_close(self):
@@ -72,6 +76,7 @@ class Wazuh(Responder):
         url_path = 'active-response'
         api_url = f"{self.wazuh_manager}/{url_path}"
         
+        # Send the active response
         response = self.session.put(
             api_url, params=self.active_response_params, 
             json=self.active_response_payload, 
@@ -80,44 +85,60 @@ class Wazuh(Responder):
         return(response)
 
 
+    def block_ip(self):
+        self.active_response_payload['command'] = '!firewall-drop'
+        self.active_response_payload['alert'] = {"data":{"srcip":self.observable}}
+
+        ar = self.active_response()
+        if ar.status_code == 200:
+            message = {'agent_id: ': self.wazuh_agent_id,
+                       'message': "Added DROP rule for ip: " + self.observable}
+            self.report(message)
+        else:
+            self.error("Request error with code:" + ar.status_code)
+    
+
+    def delete_file(self):
+        self.active_response_payload['command'] = '!firewall-drop'
+        self.active_response_payload['alert'] = {"data":{"srcip":self.observable}}
+
+        ar = self.active_response()
+        if ar.status_code == 200:
+            message = {'agent_id: ': self.wazuh_agent_id,
+                       'message': "Deleted file: " + self.observable}
+            self.report(message)
+        else:
+            self.error("Request error with code:" + ar.status_code)
+
+
     def run(self):
         Responder.run(self)
         self.login()
 
         if self.service == 'block_ip':
+            # Check if the observable is a valid IPv4/IPv6 address
             if self.observable_type == "ip":
                 try:
                     ipaddress.ip_address(self.observable)
                 except ValueError:
-                    self.error({'message': "Not a valid IPv4/IPv6 address!"})
+                    self.error("Invalid observable_type: not a valid IPv4/IPv6 address!")
             else: 
-                self.error({'message': "Not a valid IPv4/IPv6 address!"})
-
-            self.active_response_payload['command'] = '!firewall-drop'
-            self.active_response_payload['alert'] = {"data":{"srcip":"10.2.65.68"}}
-
-            ar = self.active_response()
-            with open('/opt/Custom-Analyzers/responders/Wazuh/output/test.log', 'w') as f:
-                f.write(ar.text)
-            self.report({'message': "Added DROP rule for " + self.observable})
-            logout_msg = self.logout_current_user()
+                self.error("Invalid observable_type: not a valid IPv4/IPv6 address!")
+            # Block the IP
+            self.block_ip()
         
         if self.service == 'delete_file':
             if self.observable_type == "filename":
-                parameters = {'agents_list': self.wazuh_agent_id}
-                payload = {"arguments": [ "string" ], "command": "!firewall-drop", "custom": False, "alert": {"data":{"srcip":"10.2.65.68"}}}
-
-                ar = self.active_response()
-                self.report({'message': "Deleted file: " + '"' + self.observable + '"' + " for agent " + self.wazuh_agent_id})
-                logout_msg = self.logout_current_user()
+                self.delete_file()
             else: 
-                self.error({'message': "Not a file!"})
+                self.error({"Not a file!"})
         
         self.session_close()
 
    
     def operations(self, raw):
-        return [self.build_operation('AddTagToCase', tag='Wazuh: Blocked IP')] 
+        return [self.build_operation('AddTagToCase', tag='Wazuh: Blocked IP')]
+
 
 if __name__ == '__main__':
     Wazuh().run()
